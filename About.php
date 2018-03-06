4ec79e0be963c3239bacdf1f3099e1ef1aff02f1
janellh <janellhhaynes@gmail.com>
2018-03-06 11:15:43 -0700

    php

---
+++ About.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About Labbies</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+
+<?php
+    include "Navbar.html";
+?>
+<div class="container">
+
+    <!--About heading-->
+    <h2 class="h1 m-0">About us</h2>
+    <!--Contact text-->
+    <div class="row">
+        <div class="col-lg-7">
+
+            <div class="row text-center">
+                <div class="col-xl-4">
+                    <span class="oi oi-person bg-success px-3 py-2 rounded text-white mb-2 d-inline-block"></span>
+                    <p>LuAnne Mitchell<br>Web Designer</p>
+                </div>
+
+                <div class="col-xl-4">
+                    <span class="oi oi-person bg-success px-3 py-2 rounded text-white mb-2 d-inline-block"></span>
+                    <p>Jared Rittmanic<br>Web Designer</p>
+                </div>
+
+                <div class="col-xl-4">
+                    <span class="oi oi-person bg-success px-3 py-2 rounded text-white mb-2 d-inline-block"></span>
+                    <p>JaNell Haynes<br>Web Designer</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php
+    include "Footer.html";
+?>
+</body>
+</html>
---
+++ Supplies.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Supplies Labbies</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<?php
+    include "Navbar.html";
+?>
+
+<?php
+    include "Footer.html";
+?>
+</body>
+</html>

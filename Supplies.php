7f6fb117c64b7fc5548bf98ac3eec0f47f82a1c2
janellh <janellhhaynes@gmail.com>
2018-03-08 11:13:49 -0700

    4

--- Supplies.php
+++ Supplies.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Animals Labbies</title>
+    <title>Supplies Labbies</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css" rel="stylesheet">
 </head>

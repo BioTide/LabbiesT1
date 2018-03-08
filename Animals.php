d15078c59ebe3a6f6e7ec74be850566833153370
janellh <janellhhaynes@gmail.com>
2018-03-08 11:12:50 -0700

    2

--- Animals.php
+++ Animals.php
@@ -2,12 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Supplies Labbies</title>
+    <title>Animals Labbies</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
-
 <?php
     include "Navbar.html";
 ?>
@@ -15,7 +14,7 @@
 <div class="row">
     <div class="col-xl-12">
         <h1 class = "bg-success text-white text-center">
-            Supplies
+            Animals For Sale
             <span id="shoppingCart" class="oi oi-cart align-baseline clickable col-xl-1" data-toggle="modal" data-target="#exampleModalCenter" title="See your Current Shopping Cart">
         </h1>
     </div>
@@ -25,13 +24,13 @@
 </div>
 <div class="container">
     <br>
-    <!-- Supplies Table Row -->
+    <!-- Animal Table Row -->
     <div class="row">
         <div class="col-xl-6 offset-xl-3">
-            <table class="table" id="suppliesTable">
+            <table class="table" id="animalTable">
                 <thead>
                 <tr>
-                    <th>Item</th>
+                    <th>Species</th>
                     <th class="text-right">Quantity</th>
                     <th class="text-right">Price&nbsp;&nbsp;</th>
                 </tr>
@@ -60,7 +59,7 @@
             <div class="modal-body">
                 <div class="container-fluid">
                     <div class="row">
-                        <div class="col-xl-3"><b>Item</b></div>
+                        <div class="col-xl-3"><b>Name</b></div>
                         <div class="col-xl-3"><b>Price</b></div>
                         <div class="col-xl-3"><b>Quantity</b></div>
                         <div class="col-xl-3"><b>Total</b></div>
@@ -85,21 +84,21 @@
 
 <script>
     /*global $*/
-    let supplies = [];
+    let animals = [];
     $(function(){// The DOM is ready for us to insert new data
-        $.getJSON("Supplies.json", function(data){
-            supplies = data;
+        $.getJSON("Animals.json", function(data){
+            animals = data;
             populateTable();
         });
     });
     function populateTable(){
-        //$('.supplyRow').remove();
-        for(let supply of supplies){
-            $('#suppliesTable').append(
-                `<tr class="supplyRow">
-                    <td>${supply.item}</td>
-                    <td class="text-right">${supply.quantity}&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                    <td class="text-right">${supply.price.toFixed(2)}</td>
+        //$('.animalRow').remove();
+        for(let animal of animals){
+            $('#animalTable').append(
+                `<tr class="animalRow">
+                    <td>${animal.species}</td>
+                    <td class="text-right">${animal.quantity}&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                    <td class="text-right">$${animal.price.toFixed(2)}</td>
                 </tr>`
             );
         }
--- Supplies.php
+++ Supplies.php
@@ -2,31 +2,31 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Animalsef="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" rel="stylesheet">
+    <title>Animals Labbies</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
 <?php
 include "Navbar.html";
 ?>
+
 <div class="row">
     <div class="col-xl-12">
         <h1 class = "bg-success text-white text-center">
-            Supplies
+            Animals For Sale
             <span id="shoppingCart" class="oi oi-cart align-baseline clickable col-xl-1" data-toggle="modal" data-target="#exampleModalCenter" title="See your Current Shopping Cart">
         </h1>
     </div>
     <br>
     <hr class = "style1">
     <br>
-
 </div>
 <div class="container">
     <br>
     <!-- Animal Table Row -->
     <div class="row">
-        Labbies</title>
-        <link hr <div class="col-xl-6 offset-xl-3">
+        <div class="col-xl-6 offset-xl-3">
             <table class="table" id="animalTable">
                 <thead>
                 <tr>
@@ -42,6 +42,9 @@ include "Navbar.html";
     </div><!-- End row -->
 </div><!-- End container -->
 
+<?php
+include "Footer.html";
+?>
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -61,53 +64,45 @@ include "Navbar.html";
                         <div class="col-xl-3"><b>Quantity</b></div>
                         <div class="col-xl-3"><b>Total</b></div>
                     </div> <!--ending row-->
-                    <div class="row">
-                    </div> <!--ending row-->
-                    <div class="row">
-                    </div> <!--ending row-->
-                    <div class="row">
-                    </div> <!--ending row-->
-                    <div class="row">
-                    </div> <!--ending row-->
+                    <div class="row"></div> <!--ending row-->
+                    <div class="row"></div> <!--ending row-->
+                    <div class="row"></div> <!--ending row-->
+                    <div class="row"></div> <!--ending row-->
                 </div> <!--end container-fluid-->
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     <button type="button" class="btn btn-primary">Checkout</button>
-                </div>
-            </div> <!-- end modal-content-->
-        </div>
-    </div><!--end Modal-->
-    <?php
-    include "Footer.html";
-    ?>
+                </div> <!--end modal-footer-->
+            </div> <!--end modal-body-->
+        </div> <!-- end modal-content-->
+    </div> <!--class="modal-dialog-->
+</div><!--end Modal-->
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
-    <script>
-        /*global $*/
-        let animals = [];
-        $(function(){// The DOM is ready for us to insert new data
-            $.getJSON("Animals.json", function(data){
-                animals = data;
-                populateTable();
-            });
+<script>
+    /*global $*/
+    let animals = [];
+    $(function(){// The DOM is ready for us to insert new data
+        $.getJSON("Animals.json", function(data){
+            animals = data;
+            populateTable();
         });
-        function populateTable(){
-            //$('.animalRow').remove();
-            for(let animal of animals){
-                $('#animalTable').append(
-                    `<tr class="animalRow">
+    });
+    function populateTable(){
+        //$('.animalRow').remove();
+        for(let animal of animals){
+            $('#animalTable').append(
+                `<tr class="animalRow">
                     <td>${animal.species}</td>
                     <td class="text-right">${animal.quantity}&nbsp;&nbsp;&nbsp;&nbsp;</td>
                     <td class="text-right">$${animal.price.toFixed(2)}</td>
                 </tr>`
-                );
-            }
+            );
         }
-    </script>
-
-
+    }
+</script>
 </body>
 </html>

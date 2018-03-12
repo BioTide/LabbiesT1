87cc1b05b61c1453e16ac1cc930146d05770e902
LuAnn <luanne_mitchell@msn.com>
2018-03-12 16:58:01 -0600

    Back to working

--- Home.php
+++ Home.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Home Labbies</title>
+    <link rel = "shortcut icon" href = "icon2.jpg/logo.ico">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css" rel="stylesheet">
 </head>

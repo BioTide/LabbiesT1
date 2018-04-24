1632dd58f10c6386b9c4d64e9cc689bf8a76361e
Jared <jared.rittmanic@gmail.com>
2018-04-23 19:55:44 -0600

    Added Admin Page & Navbar Link. Still working on complete functionality.

---
+++ Admin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home Labbies</title>
+    <link rel="icon" href="favicon.ico.bmp" type = "image/x-icon">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+
+<?php
+    include "Navbar.html";
+?>
+
+<div class="container">
+
+
+    <footer class = "footer text-center">
+        <p>
+        <h5>Labbies</h5>
+        77 Dog Ln, Orem, UT 84057
+        </p>
+    </footer>
+
+</div>
+<?php
+    include "Footer.html";
+?>
+</body>
+</html>

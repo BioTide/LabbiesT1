93055d2646e4673aa4c88cb720fca922a48e2eeb
Jared <jared.rittmanic@gmail.com>
2018-04-23 20:14:15 -0600

    Form Created for Admin Page

--- Admin.php
+++ Admin.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Home Labbies</title>
+    <title>Admin Labbies</title>
     <link rel="icon" href="favicon.ico.bmp" type = "image/x-icon">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css" rel="stylesheet">
@@ -15,6 +15,23 @@
 
 <div class="container">
 
+    <br>
+    <br>
+
+    <form>
+        <div class="form-group">
+            <label for="email">Username:</label>
+            <input type="email" class="form-control" id="email">
+        </div>
+        <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" id="pwd">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+
+    <br>
+    <br>
 
     <footer class = "footer text-center">
         <p>
@@ -22,7 +39,6 @@
         77 Dog Ln, Orem, UT 84057
         </p>
     </footer>
-
 </div>
 <?php
     include "Footer.html";
